67442ce3e812118f3e7260257e6db97585ffe634
anthien30 <anthien30@gmail.com>
2021-05-28 18:53:35 -0700

    MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,33 @@
 import express from 'express';
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import colors from 'colors';
 import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
 
+// default page of localhost:5000
 app.get('/', (req, res) => {
   res.send('API is running....');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+// Router mounting
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
